d0eb6bc57b5c9c5a26c7f694efa565f9128f2425
Gareth McNaboe <gareth.mcnaboe.2021@mumail.com>
2021-04-13 09:53:46 +0100

    Final version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,41 +7,37 @@ public class Main {
     //average circumference of the earth
     final static int radius = 6371;
 
-    public static void main(String[] args) {
+    public static void main (String[]args){
 
         Scanner sc = new Scanner(System.in);
+
         //Take in four inputs being lat, long x 2
         double latd1 = sc.nextDouble();
         double longd1 = sc.nextDouble();
         double latd2 = sc.nextDouble();
         double longd2 = sc.nextDouble();
 
-        //Calculate absolute difference between longtitude
+        //Calculate absolute difference between longtitude and lattitude
         double longdiffd = longd1 - longd2;
+        double latdiffd = latd1 - latd2;
 
-        //Convert from degrees to radians
+        //Convert from degrees to radians as formula takes in radians
         double longdiffr = Math.toRadians(longdiffd);
+        double latdiffr = Math.toRadians(latdiffd);
         double latr1 = Math.toRadians(latd1);
         double latr2 = Math.toRadians(latd2);
 
-        //Calculate sine and cosine from radian inputs
-        double sinlat1 = Math.sin(latr1);
-        double sinlat2 = Math.sin(latr2);
-        double coslat1 = Math.cos(latr1);
-        double coslat2 = Math.cos(latr2);
-        double coslongdiff = Math.cos(longdiffr);
+        //Haversine Formula
+        double part1 = Math.sin(latdiffr / 2) * Math.sin(latdiffr / 2);
+        double part2 = Math.cos(latr1) * Math.cos(latr2) * Math.sin(longdiffr / 2) * Math.sin(longdiffr / 2);
+        double subtotal = Math.sqrt(part1 + part2);
+        double angle = 2 * Math.asin(subtotal);
 
-        //Main formula from https://en.wikipedia.org/wiki/Great-circle distance
-        double temp = (sinlat1 * sinlat2)+(coslat1 * coslat2 * coslongdiff);
-        double angle = Math.acos(temp);
+        //Calculate the distance by mutliplying the angle at the centre of the earth by the radius of the earth
+        double distance = (angle * radius);
 
-        //multiply the angle by the radius of the earth to get the distance
-        double distance = angle * radius;
-
-        //round to nearest 100km
-        double result = Math.round((distance)/100)*100;
-
-        //print out result
-        System.out.print(result);
+        //Round to nearest 100km and print the result
+        int result = (int)Math.round((distance)/100)*100;
+        System.out.println(result);
     }
 }
